8487ef5cca1b18fff0f57a0292ce74a40590001f
Ingrid <Kelder>
2014-09-30 12:36:35 +0300

    Ulesanne 1

--- components/site-footer.php
+++ components/site-footer.php
@@ -1,3 +1,7 @@
 <footer id="site-footer">
-  <div class="footer-content">Arvestustöö: Kaspar Naaber - 26.09.2014</div>
+  <!-- <div class="footer-content">Arvestustöö: Kaspar Naaber - 26.09.2014</div> -->
+  <?php 
+  	$name = "Ingrid Kelder ";
+  	echo "<div class='footer-content'>Arvestustöö:" . $name . date("y.m.d");
+  ?>
 </footer>
--- components/site-header.php
+++ components/site-header.php
@@ -1,14 +1,29 @@
 <header id="site-header">
   <div class="site-title">
-    <h1>Arvestustöö</h1>
+    <?php
+    $site_title = "Arvestustöö";
+    echo "<h1>" . $site_title . "</h1>" ?>
   </div>
 
-  <nav class="menu-main">
+<nav class="menu-main">
+  <ul class="menu">
+  <?php
+  $menu_main = array("Avaleht", "Uudised", "Meeskond", "Harjutused");
+  echo "<li><a href=index.php>" . $menu_main[0] . "</a></li>
+  <li><a href=news.php>" . $menu_main[1] . "</a></li>
+  <li><a href=team.php>" . $menu_main[2] . "</a></li>
+  <li><a href=exercises.php>" . $menu_main[3] . "</a></li>";
+  ?>
+  </ul>
+</nav>
+
+
+<!--  <nav class="menu-main">
     <ul class="menu">
       <li><a href="index.php">Avaleht</a></li>
       <li><a href="news.php">Uudised</a></li>
       <li><a href="team.php">Meeskond</a></li>
       <li><a href="exercises.php">Harjutused</a></li>
     </ul>
-  </nav>
+  </nav> -->
 </header>
